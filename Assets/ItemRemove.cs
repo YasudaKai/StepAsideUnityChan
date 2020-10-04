085f388c996979e1f537932eae013b107e445b5d
kai.yasuda <kai0603603@gmail.com>
2020-10-04 22:44:40 +0900

    Initial commit

---
+++ Assets/ItemRemove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ItemRemove : MonoBehaviour
+{
+
+    public GameObject mainCamera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.mainCamera = GameObject.Find("Main Camera");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.transform.position.z < this.mainCamera.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
